08cb9321a2ae272975a87c128c9363550cdc33a6
Glushko-II <78683041+Glushko-II@users.noreply.github.com>
2021-02-17 23:26:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ url = f"https://www.nbrb.by/api/exrates/rates/{cur_name}?parammode=2"
 r = requests.get(url)
 usd = r.json()
 
-print(f"{usd['Cur_OfficialRate']}BYN as of {usd['Date']}")
+print(f"\n{usd['Cur_OfficialRate']}BYN as of {usd['Date']}")
