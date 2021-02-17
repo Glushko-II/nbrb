4d2b06377e08985f753e78f936fa7cd7eb9ade35
Glushko-II <78683041+Glushko-II@users.noreply.github.com>
2021-02-17 23:19:48 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+
+cur_name = input("Currency code according to ISO 4217 ")
+url = f"https://www.nbrb.by/api/exrates/rates/{cur_name}?parammode=2"
+r = requests.get(url)
+usd = r.json()
+
+print(f"{usd['Cur_OfficialRate']}BYN as of {usd['Date']}")
