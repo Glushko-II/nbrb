94aac6e945e6c0afba1fabbbe059e19a5c7f86ea
Glushko-II <78683041+Glushko-II@users.noreply.github.com>
2021-02-22 23:40:10 +0300

    Added file list_of_currencies.xls and function get_currencies_iso

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 import requests
 
-cur_name = input("Currency code according to ISO 4217 ")
-url = f"https://www.nbrb.by/api/exrates/rates/{cur_name}?parammode=2"
+
+def get_currencies_iso():
+    import xlrd
+    try:
+        book = xlrd.open_workbook("list_of_currencies.xls")
+    except FileNotFoundError:
+        return None
+    else:
+        sh = book.sheet_by_index(0)
+        return sh.col_values(colx=2, start_rowx=4, end_rowx=280)
+
+
+def get_currency():
+    pass
+
+
+def get_url(currency: str, url: str = None):
+    pass
+
+
+currency = input("Currency code according to ISO 4217 ")
+
+url = f"https://www.nbrb.by/api/exrates/rates/{currency}?parammode=2"
 r = requests.get(url)
 usd = r.json()
 
