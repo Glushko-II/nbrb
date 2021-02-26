902c65a930fa65afd0109c4dc8cf4ac3784c066d
Glushko-II <78683041+Glushko-II@users.noreply.github.com>
2021-02-26 22:59:16 +0300

    Added function get_cirrect_currency

--- main.py
+++ main.py
@@ -12,18 +12,25 @@ def get_currencies_iso():
         return sh.col_values(colx=2, start_rowx=4, end_rowx=280)
 
 
-def get_currency():
+def get_correct_currency():
+    while True:
+        currency_name = input("Currency code according to ISO 4217 ")
+        currencies_iso = get_currencies_iso()
+        if currency_name.upper() in currencies_iso:
+            break
+        else:
+            print("Incorrect currency name entered!")
+    return currency_name
+
+
+def get_url():
     pass
 
 
-def get_url(currency: str, url: str = None):
-    pass
-
-
-currency = input("Currency code according to ISO 4217 ")
+currency = get_correct_currency()
 
 url = f"https://www.nbrb.by/api/exrates/rates/{currency}?parammode=2"
 r = requests.get(url)
-usd = r.json()
+currency_rate = r.json()
 
-print(f"\n{usd['Cur_OfficialRate']}BYN as of {usd['Date']}")
+print(f"\n{currency_rate['Cur_OfficialRate']}BYN as of {currency_rate['Date']}")
